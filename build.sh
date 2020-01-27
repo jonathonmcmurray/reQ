a18bc71ed5840dfe590e9ea990cdf6a8d44e5705
Jonathon McMurray <jonathon.mcmurray@aquaq.co.uk>
2020-01-27 17:19:42 +0000

    Update build script & README

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-cat req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/auth.q req/multipart.q > req_build.q
+BUILD_VER=${1}
+
+echo Building req_${BUILD_VER}.q...
+cat req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/auth.q req/multipart.q > req_${BUILD_VER}.q
+
+echo Building req-${BUILD_VER}.tar.gz...
+mkdir req-${BUILD_VER}
+cp req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/auth.q req/multipart.q req-${BUILD_VER}/
+tar -czvf req-${BUILD_VER}.tar.gz req-${BUILD_VER}
+rm -r req-${BUILD_VER}
+
+echo Done
