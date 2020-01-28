aef2b3c527afb0aa50e5277d3894ff4d536bd21e
Jonathon McMurray <jonathon.mcmurray@aquaq.co.uk>
2020-01-28 17:56:26 +0000

    Include init.q in standalone qutil build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cat req/ext/os.q req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/aut
 
 echo Building req-${BUILD_VER}.tar.gz...
 mkdir -p req-${BUILD_VER}/ext
-cp req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/auth.q req/multipart.q req-${BUILD_VER}/
+cp req/url.q req/cookie.q req/b64.q req/status.q req/req.q req/auth.q req/multipart.q req/init.q req-${BUILD_VER}/
 cp req/ext/os.q req-${BUILD_VER}/ext/
 tar -czvf req-${BUILD_VER}.tar.gz req-${BUILD_VER}
 rm -r req-${BUILD_VER}
