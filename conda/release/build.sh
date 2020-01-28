1d9d8b23f992aaf795aa1de64e6906ee0cdbf698
Jonathon McMurray <jonathon.mcmurray@aquaq.co.uk>
2020-01-28 17:53:01 +0000

    Fix Conda recipes for ext/ dir

--- conda/release/build.sh
+++ conda/release/build.sh
@@ -4,4 +4,4 @@ PKG_NAME="req"
 
 QHOME=$PREFIX/q
 mkdir -p $QHOME/packages/${PKG_NAME}-${PKG_VERSION}
-cp -r ${SRC_DIR}/${PKG_NAME}/*.q $QHOME/packages/${PKG_NAME}-${PKG_VERSION}/
+cp -r ${SRC_DIR}/${PKG_NAME}/* $QHOME/packages/${PKG_NAME}-${PKG_VERSION}/
